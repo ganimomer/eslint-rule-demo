34ca823465f2320dbfb06e8cac5806cd92ceb42f
Omer Ganim <ganimomer@gmail.com>
2018-07-30 22:28:10 +0300

    add rule no-trainwreck

--- src/rule.js
+++ src/rule.js
@@ -1,7 +1,28 @@
 'use strict'
 
 module.exports = {
+    meta: {
+        schema: [{type: 'integer'}]
+    },
     create(context) {
-        return {}
+        const [maxDepth = 3] = context.options
+        let depth = 0
+        let shouldReport = false
+        return {
+            MemberExpression(node) {
+                depth++
+                if (depth>= maxDepth) {
+                    shouldReport = true;
+                    
+                }
+            },
+            'MemberExpression:exit'(node) {
+                depth--;
+                if (depth === 0 && shouldReport) {
+                    context.report(node, `Member chain exceeds ${maxDepth} items`)
+                    shouldReport = false
+                }
+            }
+        }
     }
 }
--- test/rule.spec.js
+++ test/rule.spec.js
@@ -3,12 +3,29 @@ const {RuleTester} = require('eslint')
 const rule = require('../src/rule')
 const ruleTester = new RuleTester();
 
-ruleTester.run('rule', rule, {
-    valid: ['foo'],
+ruleTester.run('no-trainwreck', rule, {
+    valid: [
+        'a.b.c',
+        {
+            code: 'a.b.c.d',
+            options: [4]
+        }
+    ],
     invalid: [{
-       code: 'bar',
+       code: 'a.b.c.d',
        errors: [{
-           message: 'baz'
+           message: 'Member chain exceeds 3 items'
        }]
+    }, {
+        code: 'a.b.c.d.e',
+        errors: [{
+            message: 'Member chain exceeds 3 items'
+        }]
+    }, {
+        code: 'a.b.c.d.e',
+        options: [4],
+        errors: [{
+            message: 'Member chain exceeds 4 items'
+        }]
     }]
 })
